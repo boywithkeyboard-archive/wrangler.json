07d5cd0274fcf464c16af5e37ce7dc7c6107c645
Azury <azury.wtf@gmail.com>
2022-07-28 20:50:05 +0200

    chore: rename `defineConfig` method to `define`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { defineConfig } from './modules/defineConfig'
+export { define } from './modules/define'
 export { generateConfig } from './modules/generateConfig'
 export { parseConfig } from './modules/parseConfig'
